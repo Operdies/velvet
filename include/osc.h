077003d7742de78c307af8d99f807886e819d666
alex <alexnlarsen@gmail.com>
2025-07-06 11:29:10 +0200

    Implement OSC parser

--- include/osc.h
+++ include/osc.h
@@ -3,31 +3,39 @@
 
 #include "emulator.h"
 #include <stdint.h>
+#include <unistd.h>
+
 #define OSC_MAX_PARAMS 16
 
 enum osc_fsm_state {
   OSC_GROUND,
   OSC_ACCEPT,
-  OSC_PARAMETER,
-  OSC_LEADING,
-  OSC_INTERMEDIATE,
   OSC_REJECT,
 };
 
 enum osc_command {
-  OSC_HYPERLINK_BEGIN,
-  OSC_HYPERLINK_END,
+  OSC_SET_ICON_AND_TITLE = 0,
+  OSC_SET_ICON = 1,
+  OSC_SET_TITLE = 2,
+  OSC_HYPERLINK = 8,
 };
 
-struct osc_param {
-  uint32_t primary;
-  uint8_t sub[4];
+struct osc_pt {
+  const uint8_t *text;
+  int len;
+};
+
+struct osc_parameter {
+  struct osc_pt key;
+  struct osc_pt value;
 };
 
 struct osc {
   enum osc_fsm_state state;
-  const uint8_t *buffer;
-  int len;
+  enum osc_command ps;
+  struct osc_pt pt;
+  struct osc_parameter parameters[OSC_MAX_PARAMS];
+  int n_parameters;
 };
 
 int osc_parse(struct osc *c, const uint8_t *buffer, int len);
--- src/osc.c
+++ src/osc.c
@@ -1,26 +1,104 @@
 #include "osc.h"
 #include "utils.h"
+#include <ctype.h>
 
 static bool osc_dispatch_todo(struct fsm *fsm, struct osc *osc) {
   (void)fsm;
-  TODO("OSC %.*s", osc->len, osc->buffer);
+  TODO("OSC %d %.*s", osc->ps, osc->pt.len, osc->pt.text);
   return false;
+}
+
+static bool osc_dispatch_hyperlink(struct fsm *fsm, struct osc *osc) {
+  return osc_dispatch_todo(fsm, osc);
 }
 
 bool osc_dispatch(struct fsm *fsm, struct osc *osc) {
   assert(osc->state == OSC_ACCEPT);
-  return osc_dispatch_todo(fsm, osc);
+  switch (osc->ps) {
+  case OSC_HYPERLINK: return osc_dispatch_hyperlink(fsm, osc);
+  default: return osc_dispatch_todo(fsm, osc);
+  };
+}
+
+static int osc_parse_parameters(struct osc *o, const uint8_t *buffer, int len) {
+  int i = 0;
+  if (buffer[i] != ';') {
+    logmsg("Reject OSC: Expected semicolon after Ps");
+    o->state = OSC_REJECT;
+    return i;
+  }
+  i++;
+
+  // empty parameter list
+  if (buffer[i] == ';') {
+    o->state = OSC_ACCEPT;
+    return i;
+  }
+
+  while (i < len && o->n_parameters < OSC_MAX_PARAMS) {
+    struct osc_pt key = {.text = &buffer[i]};
+    // Parameters are of the form `key1=value1:key2=value2`
+    for (; i < len && buffer[i] != '='; i++);
+    if (buffer[i] != '=') {
+      o->state = OSC_REJECT;
+      logmsg("Reject OSC: Expected '=' in parameter.");
+      return i;
+    }
+    key.len = &buffer[i] - key.text;
+    i++;
+    struct osc_pt value = {.text = &buffer[i]};
+    for (; i < len && buffer[i] != ':' && buffer[i] != ';'; i++);
+    if (buffer[i] != ':' && buffer[i] != ';') {
+      o->state = OSC_REJECT;
+      logmsg("Reject OSC: Expected parameter value to be terminated by ';' or ':'.");
+      return i;
+    }
+    value.len = &buffer[i] - value.text;
+    o->parameters[o->n_parameters] = (struct osc_parameter){.value = value, .key = key};
+    o->n_parameters++;
+    if (buffer[i] == ';') {
+      o->state = OSC_ACCEPT;
+      return i;
+    }
+    i++;
+  }
+  return 0;
 }
 
 // OSC sequences follow the syntax:
-// OSC Ps ; Pt ; BEL -- OR
-// OSC Ps ; Pt ; ST
+// OSC Ps ; Pt BEL -- OR
+// OSC Ps ; Pt ST
 // The terminator does not affect semantics of the command,
 // but should be stored for queries so the response can use the same terminator used by the query as this is likely what
 // the client expects.
 int osc_parse(struct osc *o, const uint8_t *buffer, int len) {
-  o->buffer = buffer;
-  o->len = len;
+  int ps = 0;
+
+  int i = 0;
+  for (; i < len && isdigit(buffer[i]); i++) {
+    ps *= 10;
+    ps += buffer[i] - '0';
+  }
+
+  o->ps = ps;
+
+  if (ps == 8) {
+    // OSC 8 can have optional parameters and thus use ane extra semicolon.
+    i += osc_parse_parameters(o, buffer + i, len - i);
+    if (o->state == OSC_REJECT) {
+      return i;
+    }
+  }
+
+  if (buffer[i] != ';') {
+    logmsg("Reject OSC: Expected semicolon before Pt");
+    o->state = OSC_REJECT;
+    return i;
+  }
+  i++;
+  const uint8_t *pt = &buffer[i];
   o->state = OSC_ACCEPT;
+  o->pt = (struct osc_pt){.len = len - i, .text = pt};
+
   return 0;
 }
