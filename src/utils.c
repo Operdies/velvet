1e96c4a77b19c5f228779f023a48efba6b114d28
alex <alexnlarsen@gmail.com>
2025-11-09 17:43:31 +0100

    logging: detect repeated lines with hash

--- src/utils.c
+++ src/utils.c
@@ -6,30 +6,38 @@
 #include <stdlib.h>
 #include <string.h>
 #include <termios.h>
+#include <time.h>
 #include <unistd.h>
 #include <platform.h>
+#include "murmur3.h"
 
 #define CSI "\x1b["
 
+#define MURMUR3_SEED (123) // doesn't matter
 
 static void vflogmsg(FILE *f, char *fmt, va_list ap) {
-  constexpr int bufsize = 256;
-  static char prevbuf[bufsize] = {0};
-  static char buf[bufsize] = {0};
+  constexpr int bufsize = 1 << 16;
+  static char *buf = nullptr;
+  if (!buf) {
+    buf = calloc(1, bufsize);
+  }
+  static uint32_t prev_hash = 0;
+
   static int repeat_count = 0;
   assert(f);
   assert(fmt);
   int n = strlen(fmt);
   char last = n > 0 ? fmt[n - 1] : 0;
 
-  int n_buf = vsnprintf(buf, sizeof(buf) - 1, fmt, ap);
+  int n_buf = vsnprintf(buf, bufsize - 1, fmt, ap);
   if (n_buf > bufsize) n_buf = bufsize;
 
   // Ensure at least one space
   if (last == ':') {
-    n_buf += snprintf(buf + n_buf, sizeof(buf) - n_buf, " %s", strerror(errno));
+    n_buf += snprintf(buf + n_buf, bufsize - n_buf, " %s", strerror(errno));
   }
 
+  // replace non-printable characters with dots
   for (int i = 0; i < n_buf; i++) {
     // Assume utf8 continuation byte
     if ((buf[i]) & 0x80) continue;
@@ -38,16 +46,19 @@ static void vflogmsg(FILE *f, char *fmt, va_list ap) {
     if (!isgraph(buf[i])) buf[i] = '.';
   }
 
-  if (strcmp(buf, prevbuf) == 0) {
-    repeat_count++;
-    // F: up one line, 2K: clear the line
-    fprintf(f, CSI "F" CSI "2K");
-    fprintf(f, "%.*s (%d)\n", n_buf, buf, repeat_count);
-  } else {
-    repeat_count = 1;
-    fprintf(f, "%.*s\n", n_buf, buf);
-    memcpy(prevbuf, buf, n_buf + 1);
-  }
+  time_t rawtime;
+  struct tm *timeinfo;
+  time(&rawtime);
+  timeinfo = localtime(&rawtime);
+  char timebuf[12] = {0};
+  strftime(timebuf, sizeof(timebuf), "[%H:%M:%S]", timeinfo);
+
+  uint32_t buf_hash = murmur3_32((uint8_t*)buf, bufsize, MURMUR3_SEED);
+  bool repeat = buf_hash == prev_hash;
+  prev_hash = buf_hash;
+  char *final_fmt = repeat ? ( CSI "F" CSI "2K" "%s %.*s (%d)\n" ) : ( "%s %.*s\n" );
+  repeat_count = repeat ? repeat_count + 1 : 1;
+  fprintf(f, final_fmt, timebuf, n_buf, buf, repeat_count);
   fflush(f);
 }
 
