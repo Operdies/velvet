70e7c60f79741731f32277ed3556bf9ee91ba8ee
alex <alexnlarsen@gmail.com>
2025-12-01 00:08:21 +0100

    velvet_input: parse mouse events
    now we just need to dispatch them

--- src/velvet_input.c
+++ src/velvet_input.c
@@ -1,5 +1,7 @@
 #include "velvet_input.h"
+#include "csi.h"
 #include "utils.h"
+#include "virtual_terminal_sequences.h"
 
 #define ESC 0x1b
 #define CSI '['
@@ -8,6 +10,46 @@
 
 static const struct u8_slice bracketed_paste_start = STRING_SLICE(u8"\x1b[200~");
 static const struct u8_slice bracketed_paste_end = STRING_SLICE(u8"\x1b[201~");
+
+enum scroll_direction {
+    scroll_up = 0,
+    scroll_down = 1,
+    scroll_left = 2,
+    scroll_right = 3,
+  };
+
+enum mouse_state {
+    mouse_left = 0,
+    mouse_middle = 1,
+    mouse_right = 2,
+    mouse_none = 3,
+  };
+enum mouse_modifiers {
+    modifier_none = 0,
+    modifier_shift = 0x04,
+    modifier_alt = 0x08,
+    modifier_ctrl = 0x10,
+  };
+enum mouse_event {
+    mouse_click = 0,
+    mouse_move = 0x20,
+    mouse_scroll = 0x40,
+  };
+enum mouse_trigger {
+  mouse_down,
+  mouse_up,
+};
+
+struct mouse_sgr {
+  union {
+    enum scroll_direction scroll_direction;
+    enum mouse_state button_state;
+  };
+  enum mouse_modifiers modifiers;
+  enum mouse_event event_type;
+  enum mouse_trigger trigger;
+  int row, column;
+};
 
 static void send(struct velvet_input *in, struct u8_slice s) {
   struct vte_host *focus = vec_nth(&in->m->hosts, in->m->focus);
@@ -33,9 +75,72 @@ static void dispatch_normal(struct velvet_input *in, uint8_t ch) {
   }
 }
 
-static void send_csi(struct velvet_input *in) {
+static void send_csi_todo(struct velvet_input *in) {
   TODO("Input CSI: %.*s", in->command_buffer.len, in->command_buffer.content);
   string_clear(&in->command_buffer);
+}
+
+static void mouse_debug_logging( struct mouse_sgr sgr) {
+  char *mousebuttons[] = {"left", "middle", "right", "none"};
+  char *mousebutton = mousebuttons[sgr.button_state];
+  char *scrolldirs[] = {"up", "down", "left", "right"};
+  char *scrolldir = scrolldirs[sgr.scroll_direction];
+  char *eventname = sgr.event_type & mouse_scroll ? "scroll"
+                    : sgr.event_type & mouse_move ? "move"
+                    : sgr.trigger == mouse_down   ? "click"
+                                                  : "release";
+
+  logmsg("shift=%d,alt=%d,ctrl=%d",
+         !!(sgr.modifiers & modifier_shift),
+         !!(sgr.modifiers & modifier_alt),
+         !!(sgr.modifiers & modifier_ctrl));
+  switch (sgr.event_type) {
+  case mouse_click: logmsg("%s %s at %d;%d", eventname, mousebutton, sgr.row, sgr.column); break;
+  case mouse_move: logmsg("%s at %d;%d", eventname, sgr.row, sgr.column); break;
+  case mouse_scroll: logmsg("%s %s %d;%d", eventname, scrolldir, sgr.row, sgr.column); break;
+  }
+}
+
+struct mouse_sgr mouse_sgr_from_csi(struct csi c) {
+  int btn = c.params[0].primary;
+  int col = c.params[1].primary;
+  int row = c.params[2].primary;
+
+  enum mouse_state mouse_button = btn & 0x03;
+  enum mouse_modifiers modifiers = btn & (0x04 | 0x08 | 0x10);
+  enum mouse_event event_type = btn & (0x20 | 0x40);
+
+  struct mouse_sgr sgr = {
+      .button_state = mouse_button,
+      .modifiers = modifiers,
+      .event_type = event_type,
+      .row = row,
+      .column = col,
+      .trigger = c.final == 'M' ? mouse_down : mouse_up,
+  };
+  return sgr;
+}
+
+static void send_csi_mouse(struct velvet_input *in, struct csi c) {
+  struct vte_host *focus = vec_nth(&in->m->hosts, in->m->focus);
+  struct mouse_options m = focus->vte.options.mouse;
+
+  struct mouse_sgr sgr = mouse_sgr_from_csi(c);
+
+  mouse_debug_logging(sgr);
+
+  }
+
+  if (m.tracking == MOUSE_TRACKING_OFF || m.tracking == MOUSE_TRACKING_LEGACY) 
+    return;
+
+  switch (m.tracking) {
+  case MOUSE_TRACKING_OFF:
+  case MOUSE_TRACKING_LEGACY: return;
+  case MOUSE_TRACKING_CLICK: break;
+  case MOUSE_TRACKING_CELL_MOTION: break;
+  case MOUSE_TRACKING_ALL_MOTION: break;
+  }
 }
 
 static void dispatch_csi(struct velvet_input *in, uint8_t ch) {
@@ -55,7 +160,26 @@ static void dispatch_csi(struct velvet_input *in, uint8_t ch) {
 
   // TODO: Is this accurate?
   if (ch >= 0x40 && ch <= 0x7E) {
-    send_csi(in);
+    struct csi c = {0};
+    struct u8_slice s = string_range(&in->command_buffer, 2, -1);
+    size_t len = csi_parse(&c, s);
+    if (c.state == CSI_ACCEPT) {
+      assert(len == s.len);
+      switch (c.final) {
+      case 'I': {
+        send(in, vt_focus_in);
+      } break;
+      case 'O': {
+        send(in, vt_focus_out);
+      } break;
+      case 'm':
+      case 'M': {
+        send_csi_mouse(in, c);
+      } break;
+      default: send_csi_todo(in); break;
+      }
+    }
+    string_clear(&in->command_buffer);
     in->state = VELVET_INPUT_STATE_NORMAL;
   }
 }
