6fd5ef83b01fbf5c5a03865dbdc645f012e75b32
alex <alexnlarsen@gmail.com>
2025-12-01 10:31:22 +0100

    vt sequences: distinguish ansi and private modes

--- include/virtual_terminal_sequences.h
+++ include/virtual_terminal_sequences.h
@@ -8,7 +8,12 @@
 
 #define VT(name, command) static const struct u8_slice vt_##name = {.content = command, .len = sizeof(command) - 1}
 
-#define VT_MODE(name, mode)                                                                                            \
+#define VT_ANSI_MODE(name, mode)                                                                                            \
+  static const struct u8_slice vt_##name##_on = {.content = CSI #mode "h", .len = sizeof(CSI "?" #mode "h") - 1};    \
+  static const struct u8_slice vt_##name##_off = {.content = CSI #mode "l", .len = sizeof(CSI "?" #mode "l") - 1};   \
+  static const struct u8_slice vt_##name##_query = {.content = CSI #mode "$p", .len = sizeof(CSI "?" #mode "$p") - 1};
+
+#define VT_PRIVATE_MODE(name, mode)                                                                                            \
   static const struct u8_slice vt_##name##_on = {.content = CSI "?" #mode "h", .len = sizeof(CSI "?" #mode "h") - 1};    \
   static const struct u8_slice vt_##name##_off = {.content = CSI "?" #mode "l", .len = sizeof(CSI "?" #mode "l") - 1};   \
   static const struct u8_slice vt_##name##_query = {.content = CSI "?" #mode "$p", .len = sizeof(CSI "?" #mode "$p") - 1};
@@ -19,14 +24,14 @@ VT(focus_out, CSI "O");
 VT(focus_in, CSI "I");
 VT(clear, CSI "2J");
 
-VT_MODE(synchronized_rendering, 2026);
-VT_MODE(mouse_mode_sgr, 1006);
-VT_MODE(mouse_mode_sgr_pixel, 1016);
-VT_MODE(mouse_tracking, 1003);
-VT_MODE(line_wrapping, 7);
-VT_MODE(cursor_visible, 7);
-VT_MODE(focus_reporting, 1004);
-VT_MODE(bracketed_paste, 2004);
+VT_PRIVATE_MODE(synchronized_rendering, 2026);
+VT_PRIVATE_MODE(mouse_mode_sgr, 1006);
+VT_PRIVATE_MODE(mouse_mode_sgr_pixel, 1016);
+VT_PRIVATE_MODE(mouse_tracking, 1003);
+VT_PRIVATE_MODE(line_wrapping, 7);
+VT_PRIVATE_MODE(cursor_visible, 7);
+VT_PRIVATE_MODE(focus_reporting, 1004);
+VT_PRIVATE_MODE(bracketed_paste, 2004);
 
 #undef VT
 #undef ESC
