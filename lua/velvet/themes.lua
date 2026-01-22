f3cf853e9dd04af592749a693ee558b9454bb7fb
alex <alexnlarsen@gmail.com>
2026-01-22 08:05:48 +0100

    add gruvbox theme

--- lua/velvet/themes.lua
+++ lua/velvet/themes.lua
@@ -1,22 +1,61 @@
 return {
   catppuccin = {
     mocha = {
+      -- ansi colors 0-7 (fg 30-37, bg 40-47)
       black = "#45475a",
-      blue = "#89b4fa",
-      bright_black = "#585b70",
-      bright_blue = "#89b4fa",
-      bright_cyan = "#94e2d5",
-      bright_green = "#a6e3a1",
-      bright_magenta = "#f5c2e7",
-      bright_red = "#f38ba8",
-      bright_white = "#a6adc8",
-      bright_yellow = "#f9e2af",
-      cyan = "#94e2d5",
-      green = "#a6e3a1",
-      magenta = "#f5c2e7",
       red = "#f38ba8",
+      green = "#a6e3a1",
+      yellow = "#f9e2af",
+      blue = "#89b4fa",
+      magenta = "#f5c2e7",
+      cyan = "#94e2d5",
       white = "#bac2de",
-      yellow = "#f9e2af"
+
+      -- ansi colors 8-15 (fg 90-97, bg 100-107)
+      bright_black = "#585b70",
+      bright_red = "#f38ba8",
+      bright_green = "#a6e3a1",
+      bright_yellow = "#f9e2af",
+      bright_blue = "#89b4fa",
+      bright_magenta = "#f5c2e7",
+      bright_cyan = "#94e2d5",
+      bright_white = "#a6adc8",
+
+      -- background color used when no color is specified (default for most applications)
+      background = "#1e1e2e",
+      -- text color when no oclor is specified
+      foreground = "#cdd6f4",
+      -- background color of the cell containing the cursor, if block cursor is used
+      cursor_background = "#f5e0dc",
+      -- text color of the cell containing the cursor, if block cursor is used
+      cursor_foreground = "#1e1e2e",
     }
-  }
+  },
+  gruvbox = {
+    dark = {
+      black          = '#282828',
+      red            = '#cc241d',
+      green          = '#98971a',
+      yellow         = '#d79921',
+      blue           = '#458588',
+      magenta        = '#b16286',
+      cyan           = '#689d6a',
+      white          = '#a89984',
+
+      bright_black   = '#928374',
+      bright_red     = '#fb4934',
+      bright_green   = '#b8bb26',
+      bright_yellow  = '#fabd2f',
+      bright_blue    = '#83a598',
+      bright_magenta = '#d3869b',
+      bright_cyan    = '#8ec07c',
+      bright_white   = '#ebdbb2',
+
+      background     = '#282828',
+      foreground     = '#ebdbb2',
+
+      cursor_background = "#ebdbb2",
+      cursor_foreground = "#282828",
+    },
+  },
 }
