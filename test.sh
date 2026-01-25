04a6a0a3e264f8ef5d015456a0daf98d21431af7
alex <alexnlarsen@gmail.com>
2026-01-25 10:09:41 +0100

    test script: set pipefail

--- test.sh
+++ test.sh
@@ -2,11 +2,13 @@
 
 cd "$(dirname "$0")"
 
+set -euo pipefail
+
 echo =========== Build Debug ==============
 make -j
-echo =========== Build Release ============
-make -j release
 echo =========== Test Debug ===============
 ./bin/test
+echo =========== Build Release ============
+make -j release
 echo =========== Test Release =============
 ./release/test
