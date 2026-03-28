05c40f95d519f98b31e1f134c23231c8ecc5bcfc
alex <alexnlarsen@gmail.com>
2026-03-28 21:02:24 +0100

    server: limit caps

--- web/entrypoint.sh
+++ web/entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-cp -a /etc/skel/. ~/
-exec /home/demo/velvet/release/vv
+chown demo:demo /home/demo
+su -s /bin/sh demo -c 'cp -a /etc/skel/. ~/'
+exec su -s /bin/bash -l demo -c 'exec /home/demo/velvet/release/vv'
