66b9f38a73d67d9a5fcc6b31b2a4c8d7287957c5
alex <alexnlarsen@gmail.com>
2026-07-20 16:38:55 +0200

    refactor(vv): simplify lua vm initialization logic

--- src/velvet_lua.c
+++ src/velvet_lua.c
@@ -60,70 +60,6 @@ void velvet_lua_restart_vm(void *data) {
   velvet_source_config(v);
 }
 
-int lua_debug_traceback_handler(lua_State *L) {
-  luaL_traceback(L, L, lua_tostring(L, 1), 1);
-  return 1;
-}
-
-// static int l_velvet_log(lua_State *L) {
-//   const char *str = luaL_checkstring(L, 1);
-//   velvet_log("lua: %s", str);
-//   return 0;
-// }
-
-static void velvet_lua_init_coroutine_helper(struct velvet *v) {
-  char coroutine_helper[] = {
-      "return function(chunk, setup, cleanup, print_function, cwd, arg0, ...)\n"
-      "  local cli_args = {...}\n"
-      "  cli_args[0] = arg0\n"
-      "  vv.async.run(function()\n"
-      "    setup()\n"
-      "    COROUTINE_PRINT[coroutine.running()] = print_function\n"
-      "    COROUTINE_ARGS[coroutine.running()] = cli_args\n"
-      "    COROUTINE_CWD[coroutine.running()] = cwd\n"
-      "    local ok, result = xpcall(chunk, debug.traceback)\n"
-      "    if result then result = type(result) == 'string' and result or require('velvet.json').to_json(result) end\n"
-      "    if not ok then\n"
-      "      print_function(2, 'Unhandled error in lua chunk: ' .. result)\n"
-      "    elseif result ~= nil then\n"
-      "      print_function(1, result)\n"
-      "    end\n"
-      "    cleanup(ok and true or false)\n"
-      "  end)\n"
-      "end",
-  };
-  int status = luaL_loadbuffer(v->L, coroutine_helper, strlen(coroutine_helper), "@velvet.cli");
-  lua_call(v->L, 0, 1);
-  assert(status == LUA_OK);
-  assert(lua_type(v->L, -1) == LUA_TFUNCTION);
-  v->coroutine_wrapper_function = luaL_ref(v->L, LUA_REGISTRYINDEX);
-}
-
-static void velvet_lua_init_arg(struct velvet *v) {
-  lua_State *L = v->L;
-  lua_getglobal(L, "vv");
-  lua_newtable(L);
-  int i = 1;
-  /* push velvet binary to arg[0]. The lua interpreter uses arg[0]
-   * to set the script path with e.g. `lua myscript.lua`,
-   * so this should be similar. */
-  lua_pushstring(L, v->arg0);
-  lua_rawseti(L, -2, 0);
-  char **arg = v->positional_args;
-  for (;arg && *arg; arg++, i++) {
-    lua_pushstring(L, *arg);
-    lua_seti(L, -2, i);
-  }
-  lua_setfield(L, -2, "startup_arguments");
-}
-
-static void velvet_lua_set_default_options(struct velvet *v) {
-  lua_State *L = v->L;
-  if (luaL_dostring(L, "require('velvet.default_options')") != LUA_OK)
-    lua_die(L);
-  lua_pop(L, lua_gettop(L));
-}
-
 static bool file_exists(const char *path) {
   struct stat st;
   return stat(path, &st) == 0;
@@ -159,16 +95,6 @@ void velvet_source_config(struct velvet *v) {
   string_destroy(&scratch);
 }
 
-int luaopen_velvet_api(lua_State *L);
-static void velvet_lua_init_api(struct velvet *v) {
-  /* vv.api = luaopen_velvet_api() */
-  lua_State *L = v->L;
-  lua_getglobal(L, "vv"); /* vv */
-  luaopen_velvet_api(L); /* vv, api */
-  lua_setfield(L, -2, "api"); /* vv.api = api */
-  lua_pop(L, 1); /* pop vv */
-}
-
 void velvet_lua_init(struct velvet *v) {
   assert(!v->L);
   lua_State *L = lua_newstate(lua_allocator, NULL, 0);
@@ -177,28 +103,69 @@ void velvet_lua_init(struct velvet *v) {
   struct velvet **extra = lua_getextraspace(v->L);
   *extra = v;
 
-  /* The lua distribution is in the parent directory of the folder containing the vv binaries.
+  { /* load api functions */
+    int luaopen_velvet_api(lua_State * L);
+    luaopen_velvet_api(L);
+    lua_setglobal(L, "API");
+  }
+
+  { /* setup argv[] table */
+    lua_newtable(L);
+    int i = 1;
+    /* push velvet binary to arg[0]. The lua interpreter uses arg[0]
+     * to set the script path with e.g. `lua myscript.lua`,
+     * so this should be similar. */
+    lua_pushstring(L, v->arg0);
+    lua_rawseti(L, -2, 0);
+    char **arg = v->positional_args;
+    for (; arg && *arg; arg++, i++) {
+      lua_pushstring(L, *arg);
+      lua_seti(L, -2, i);
+    }
+    lua_setglobal(L, "ARGS");
+  }
+
+  /* The lua distribution is in the parent directory of the folder containing the vv binary.
    * We don't want to load lua libraries from any other directories by default.
    * End users can manually add search paths if they want to. */
-  if (luaL_dostring(L, "package.path = '../lua/?/init.lua;../lua/?.lua'") != LUA_OK) {
+  char init[] = {
+      "package.path = '../lua/?/init.lua;../lua/?.lua'\n"
+      "_G.vv = require('velvet')\n"
+      "vv.api, API = API, nil\n"
+      "vv.startup_arguments, ARGS = ARGS, nil\n"
+      "require('velvet.default_options')\n",
+  };
+
+  if (luaL_dostring(L, init) != LUA_OK) {
     lua_die(L);
   }
 
-  if (luaL_dostring(L, "package.cpath = './?.so'") != LUA_OK) {
-    lua_die(L);
+  { /* create cli wrapper function */
+    char coroutine_helper[] = {
+        "local function wrap(chunk, setup, cleanup, print, cwd, args)\n"
+        "  setup()\n"
+        "  COROUTINE_PRINT[coroutine.running()] = print\n"
+        "  COROUTINE_ARGS[coroutine.running()] = args\n"
+        "  COROUTINE_CWD[coroutine.running()] = cwd\n"
+        "  local ok, result = xpcall(chunk, debug.traceback)\n"
+        "  if result then result = type(result) == 'string' and result or require('velvet.json').to_json(result) end\n"
+        "  if not ok then\n"
+        "    print(2, 'Unhandled error in lua chunk: ' .. result)\n"
+        "  elseif result ~= nil then\n"
+        "    print(1, result)\n"
+        "  end\n"
+        "  cleanup(ok and true or false)\n"
+        "end\n"
+        "return function(chunk, setup, cleanup, print, cwd, arg0, ...)\n"
+        "  vv.async.run(wrap, chunk, setup, cleanup, print, cwd, { [0] = arg0, ... })\n"
+        "end",
+    };
+    int status = luaL_loadbuffer(v->L, coroutine_helper, strlen(coroutine_helper), "@velvet.cli");
+    lua_call(v->L, 0, 1);
+    assert(status == LUA_OK);
+    assert(lua_type(v->L, -1) == LUA_TFUNCTION);
+    v->coroutine_wrapper_function = luaL_ref(v->L, LUA_REGISTRYINDEX);
   }
-
-  /* set `vv` in the global scope */
-  if (luaL_dostring(L, "vv = require('velvet')") != LUA_OK) {
-    lua_die(L);
-  }
-
-  velvet_lua_init_api(v);
-  /* set args from command line */
-  velvet_lua_init_arg(v);
-  velvet_lua_init_coroutine_helper(v);
-  // velvet_lua_init_log(v);
-  velvet_lua_set_default_options(v);
 }
 
 static void vv_log_lua_error(struct velvet *vv) {
